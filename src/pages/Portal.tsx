da5ab9c799649a93e36883d9c70ee38b772e330e
Tomaks <ctomaque02@gmail.com>
2026-04-24 11:13:07 +0800

    adj logo

--- src/pages/Portal.tsx
+++ src/pages/Portal.tsx
@@ -173,7 +173,7 @@ export default function Portal() {
             <img 
               src={cdmLogo} 
               alt="CDM Logo" 
-              className="w-12 h-12 sm:w-20 sm:h-20 -mt-1 object-contain shrink-0 drop-shadow-md" 
+              className="w-12 h-12 sm:w-20 sm:h-20 -mt-3 object-contain shrink-0 drop-shadow-md" 
             />
             
             {/* Center Text */}
@@ -191,7 +191,7 @@ export default function Portal() {
             <img 
               src={osaLogo} 
               alt="OSA Logo" 
-              className="w-12 h-12 sm:w-20 sm:h-20 -mt-1 object-contain shrink-0 drop-shadow-md scale-170 origin-top" 
+              className="w-12 h-12 sm:w-20 sm:h-20 -mt-4 object-contain shrink-0 drop-shadow-md scale-170 origin-top" 
             />
 
           </div>
